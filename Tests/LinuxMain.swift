b4bf14b026c678f5c34c142f48d904649eb3b257
Gabriel Lanata <gabriellanata@gmail.com>
2016-10-14 19:52:25 -0500

    Initial Commit

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import StartAppsKitXmlTests
+
+XCTMain([
+     testCase(StartAppsKitXmlTests.allTests),
+])
---
+++ Tests/StartAppsKitXmlTests/StartAppsKitXmlTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import StartAppsKitXml
+
+class StartAppsKitXmlTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(StartAppsKitXml().text, "Hello, World!")
+    }
+
+
+    static var allTests : [(String, (StartAppsKitXmlTests) -> () throws -> Void)] {
+        return [
+            ("testExample", testExample),
+        ]
+    }
+}
